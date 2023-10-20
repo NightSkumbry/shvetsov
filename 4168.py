03798d9800cf280b3bc2e4cbff29a083c702b463
NightSkumbry <night_skumbry@outlook.com>
2023-10-20 23:17:50 +0700

    20.10.23

--- 4168.py
+++ 4168.py
@@ -1,10 +1,9 @@
 a = abs(7**500+7**200-7**50)
 m = 0
-for x in range(1, a):
-    b = a-x
-    p = []
-    while b > 0:
-        p.append(b%7)
-        b//=7
-    m = max(m, sum(p))
-print(m)
+
+p = []
+while a > 0:
+    p.append(a%7)
+    a//=7
+
+print(6*(len(p)-1))
---
+++ 4922.py
@@ -0,0 +1,32 @@
+def A():
+    with open('./files/4922_a.txt') as f:
+        a = f.read().strip().split('\n')
+        d = int(a[0].split()[1])
+        a = list(map(int, a[1:]))
+    ans = 0
+    s = 0
+    b = []
+    for i in a:
+        s += i
+        ans += s%d == 0
+        for k in b:
+            ans += (s-k)%d == 0
+        b.append(s)
+    print(ans)
+
+def B():
+    with open('./files/4922_b.txt') as f:
+        a = f.read().strip().split('\n')
+        d = int(a[0].split()[1])
+        a = list(map(int, a[1:]))
+    ans = 0
+    s = 0
+    b = [0 for i in range(d)]
+    for i in a:
+        s += i
+        ans += s%d == 0
+        ans += b[s%d]
+        b[s%d] += 1
+    print(ans)
+
+B()
