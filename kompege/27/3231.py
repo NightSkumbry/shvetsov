6d6ff3bb42ae57ae54f8bda91a273f51c1a6f05f
NightSkumbry <night_skumbry@outlook.com>
2023-11-28 02:27:58 +0700

    .

--- kompege/27/3231.py
+++ kompege/27/3231.py
@@ -1,5 +1,6 @@
+from itertools import accumulate
 
-def v(n):
+def v(n, g, a):
     ans = 0
     for i in range(g+1):
         ans += i * a[(i+n)%len(a)]
@@ -8,32 +9,54 @@ def v(n):
     return ans
 
 
+def get_sum(pr, s, e):
+    if e > s:
+        return pr[e]-pr[s]
+    return pr[-1]-pr[s] + pr[e]
 
-with open('./files/27-A_3231.txt') as f:
-    a = list(map(int, f.read().strip().split('\n')[1:]))
-g = len(a)//2
-a3 = [2*a[i]-a[(i+g)%len(a)]-a[(i-g)%len(a)] for i in range(len(a))]
-a1 = [v(0), v(1)]
-a2 = [a1[1]-a1[0]]
-for i in a3[1:]:
-    a2.append(a2[-1]+i)
-for i in a2[1:-1]:
-    a1.append(a1[-1]+i)
 
-print(a1.index(min(a1))+1)
-# print(list(map(lambda x: v(x[0]), enumerate(a))), a1, a2, a3, sep='\n')
+def A():
+    with open('./files/27-A_3231.txt') as f:
+        a = list(map(int, f.read().strip().split('\n')[1:]))
+    g = len(a)//2
+    a1 = [v(i, g, a) for i in range(len(a))]
+    
 
-with open('./files/27-B_3231.txt') as f:
-    a = list(map(int, f.read().strip().split('\n')[1:]))
-g = len(a)//2
-a3 = [2*a[i]-a[(i+g)%len(a)]-a[(i-g)%len(a)] for i in range(len(a))]
-a1 = [v(0), v(1)]
-a2 = [a1[1]-a1[0]]
-for i in a3[1:]:
-    a2.append(a2[-1]+i)
-for i in a2[1:-1]:
-    a1.append(a1[-1]+i)
+    print(a1.index(min(a1))+1)
 
-print(a1.index(min(a1))+1)
 
+def B1():
+    with open('./files/27-B_3231.txt') as f:
+        a = list(map(int, f.read().strip().split('\n')[1:]))
+    g = len(a)//2
+    a3 = [2*a[i]-a[(i+g)%len(a)]-a[(i-g)%len(a)] for i in range(len(a))]
+    a1 = [v(0, g, a), v(1, g, a)]
+    a2 = [a1[1]-a1[0]]
+    for i in a3[1:]:
+        a2.append(a2[-1]+i)
+    for i in a2[1:-1]:
+        a1.append(a1[-1]+i)
+
+    print(a1.index(min(a1))+1)
+
+
+def B2():
+    with open('./files/27-B_3231.txt') as f:
+        a = list(map(int, f.read().strip().split('\n')[1:]))
+    g = len(a)//2
+    ans = 0
+    p = h = v(0, g, a)
+    pr = [0] + list(accumulate(a))
+    for i in range(1, len(a)):
+        h += get_sum(pr, (i-g)%len(a), i) - get_sum(pr, i, (i+g)%len(a))
+        if h < p:
+            p = h
+            ans = i
+    print(ans+1)
+
+
+import timeit
+print(timeit.timeit(A, number=1))
+print(timeit.timeit(B1, number=1))
+print(timeit.timeit(B2, number=1))
 
