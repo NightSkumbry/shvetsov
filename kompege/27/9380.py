887482d4a39d0ca7d06ec1501897723fcb8f6f3e
NightSkumbry <night_skumbry@outlook.com>
2023-11-28 01:55:58 +0700

    .

--- kompege/27/9380.py
+++ kompege/27/9380.py
@@ -1,20 +1,12 @@
+from itertools import accumulate
 import math
 
-def F1(a, n, p):
-    ans = 0
-    for i in range(1, n+1):
-        ans += i*a[(p+i)%len(a)]
-        ans += i*a[(p-i)%len(a)]
-    if len(a) % 2 == 0:
-        ans -= n*a[(p+n)%len(a)]
-    return ans
 
-
-def F2(a, n, p):
-    ans = 0
-    for i in range(1, n):
-        ans += i*a[(p+i)%len(a)]
-    return ans
+def get_sum(pr, s, e):
+    if e > s:
+        return pr[e]-pr[s]
+    else:
+        return pr[-1]-pr[s] + pr[e]
 
 
 def A():
@@ -47,15 +39,32 @@ def B():
         a = f.read().strip().split('\n')
         k = int(a[0].split()[1])
         a = list(map(lambda x: math.ceil(int(x)/k), a[1:]))
-    n = len(a)//2
-    
-    a3 = [a[i]*2-a[(i-n)%len(a)]-a[(i+n)%len(a)] for i in range(len(a))]
-    a1 = [F1(a, n, 0), F1(a, n, 1)]
-    a2 = [a1[1]-a1[0]]
-    for i in a3[1:]:
-        a2.append(a2[-1]+i)
-    for i in a2[1:-1]:
-        a1.append(a1[-1]+i)
+    i2 = 0
+    i1 = 0
+    k1 = 0
+    k2 = 0
+    ans = 0
+    for j in range(len(a)-1):
+        if k1 > k2:
+            i2 += 1
+            k2 += i2*a[(-i2)%len(a)]
+        else:
+            i1 += 1
+            k1 += i1*a[(i1)%len(a)]
+    pr = [0] + list(accumulate(a))
+    for i in range(1, len(a)):
+        k1 -= get_sum(pr, i, (i+i1)%len(a))
+        k2 += get_sum(pr, (i-i2-1)%len(a), i)
+        i1 -= 1
+        i2 += 1
+        while k2 > k1:
+            k2 -= i2*a[(i-i2)%len(a)]
+            i2 -= 1
+            i1 += 1
+            k1 += i1*a[(i+i1)%len(a)]
+        if k1 == k2:
+            ans = i
+    print(ans)
 
     
     
