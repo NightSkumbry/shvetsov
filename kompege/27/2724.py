4d2687b336e6a218868b87c63253041f467daaf8
NightSkumbry <night_skumbry@outlook.com>
2023-11-28 22:45:11 +0700

    .

--- kompege/27/2724.py
+++ kompege/27/2724.py
@@ -1,31 +1,33 @@
-with open('./files/27A_2724.txt') as f:
-    a = list(map(int, f.read().strip().split('\n')[1:]))
-b = [0 for i in range(131)]
-for i in a:
-    b[i%131] += 1
+def A():
+    with open('./files/27A_2724.txt') as f:
+        a = list(map(int, f.read().strip().split('\n')[1:]))
+    ans = 0
+    for i in range(len(a)):
+        for j in range(i+1, len(a)):
+            if (a[j] + a[i]) % 131 == 0:
+                ans += 1
 
-ans = 0
-for i in a:
-    b[i%131] -= 1
-    ans += b[(131-i%131)%131]
-
-print(ans)
-
-with open('./files/27B_2724.txt') as f:
-    a = list(map(int, f.read().strip().split('\n')[1:]))
-b = [0 for i in range(131)]
-for i in a:
-    b[i%131] += 1
-
-ans = 0
-for i in a:
-    b[i%131] -= 1
-    ans += b[(131-i%131)%131]
-
-print(ans)
+    print(ans)
 
 
+def B():
+    with open('./files/27B_2724.txt') as f:
+        a = list(map(int, f.read().strip().split('\n')[1:]))
+    k = [0 for i in range(131)]
+    for i in a:
+        k[i%131] += 1
 
+    ans = 0
+    for i in a:
+        k[i%131] -= 1
+        ans += k[(131-i%131)%131]
+
+    print(ans)
+
+
+import timeit
+print(timeit.timeit(A, number=1))
+print(timeit.timeit(B, number=1))
 
 
 
